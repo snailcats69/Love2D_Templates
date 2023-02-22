2b0212b50425ae17f08a0a8a3e6b81fa6c0d974f
JuicyWetBalls8 <124236227+JuicyWetBalls8@users.noreply.github.com>
2023-02-22 15:35:22 +0600

    Update 2d_camera.lua

--- 2d_camera.lua
+++ 2d_camera.lua
@@ -7,20 +7,7 @@ function Camera.new(x,y,angle,zoom)
   self.angle = angle or 0
   self.zoom = zoom or 1
   
-  function love.mousemoved(x, y, dx, dy)
-    if love.mouse.isDown(2) then
-        self.x = self.x - dx / self.zoom
-        self.y = self.y - dy / self.zoom
-    end
-  end
 
-  function love.wheelmoved(x, y)
-      local scale_change = y * 0.1
-      if self.zoom * (1 + scale_change) > 0.5 then
-          self.zoom = self.zoom * (1 + scale_change)
-      end
-  end
-  
   return self
 end
 
@@ -32,6 +19,19 @@ function Camera:set_position(x,y) self.x,self.y=x,y end
 function Camera:set_angle(a) self.angle=a end
 function Camera:set_zoom(a) self.zoom=a end
 
+function Camera:mousemoved(x, y, dx, dy)
+    if love.mouse.isDown(2) then
+        self.x = self.x - dx / self.zoom
+        self.y = self.y - dy / self.zoom
+    end
+end
 
+function Camera:wheelmoved(x, y)
+    local scale_change = y * 0.1
+    if self.zoom * (1 + scale_change) > 0.5 then
+        self.zoom = self.zoom * (1 + scale_change)
+    end
+end
+  
 
 return Camera
